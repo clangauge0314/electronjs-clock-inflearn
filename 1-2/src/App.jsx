06085eb82779544c284d495359c43d2b58c9a730
clangauge0314 <clangauge0314@naver.com>
2025-05-29 17:48:58 +0900

    Electron.js-Inflearn

---
+++ 1-2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="p-4 bg-blue-100 text-center">
+      <h1 className="text-xl font-semibold text-blue-700">Hello Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
